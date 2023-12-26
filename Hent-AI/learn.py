a81f6fab6b6be2cb20af0779a8e423d8dea6e9d6
�Hispalus1 <luki.hrna@seznam.cz>
2023-12-27 00:09:05 +0100

    testing pytorch 1

--- Hent-AI/learn.py
+++ Hent-AI/learn.py
@@ -1,6 +1,32 @@
 import torch
+# Create a tensor
+import random
+# Set device type
+device = "cuda" if torch.cuda.is_available() else "cpu"
 
-import torch
-x = torch.rand(5, 3)
-print(x)
-print(torch.__version__)
+print(device)
+print(torch.cuda.device_count())
+# Create two random tensors
+random_tensor_A = torch.rand(3, 4)
+random_tensor_B = torch.rand(3, 4)
+
+print(f"Tensor A:\n{random_tensor_A}\n")
+print(f"Tensor B:\n{random_tensor_B}\n")
+print(f"Does Tensor A equal Tensor B? (anywhere)")
+print(random_tensor_A == random_tensor_B) 
+
+
+# # Set the random seed
+RANDOM_SEED=42 # try changing this to different values and see what happens to the numbers below
+torch.manual_seed(seed=RANDOM_SEED) 
+random_tensor_C = torch.rand(3, 4)
+
+# Have to reset the seed every time a new rand() is called 
+# Without this, tensor_D would be different to tensor_C 
+torch.random.manual_seed(seed=RANDOM_SEED) # try commenting this line out and seeing what happens
+random_tensor_D = torch.rand(3, 4)
+
+print(f"Tensor C:\n{random_tensor_C}\n")
+print(f"Tensor D:\n{random_tensor_D}\n")
+print(f"Does Tensor C equal Tensor D? (anywhere)")
+print(random_tensor_C == random_tensor_D)
