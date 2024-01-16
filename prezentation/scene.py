f13ff04af20229d29ca821b0bdc678b34e3ee588
�Hispalus1 <luki.hrna@seznam.cz>
2024-01-16 21:37:00 +0100

    vykresleniObrazkuProPrezentaci

--- prezentation/scene.py
+++ prezentation/scene.py
@@ -1,38 +1,43 @@
-from manim import *
+import pygame
+import sys
 
-class IntroNeuralNetworks(Scene):
-    def construct(self):
-        # Title for the section
-        title = Text("Introduction to Neural Networks", font_size=24)
-        self.play(Write(title))
-        self.wait(1)
-        self.play(FadeOut(title))
+# Inicializace Pygame
+pygame.init()
 
-        # Creating a basic neural network diagram
-        neurons_layer1 = VGroup(*[Circle() for _ in range(3)]).arrange(RIGHT, buff=1)
-        neurons_layer2 = VGroup(*[Circle() for _ in range(4)]).arrange(RIGHT, buff=1)
-        neurons_layer3 = VGroup(*[Circle() for _ in range(2)]).arrange(RIGHT, buff=1)
+# Nastavení velikosti okna a fontu
+width, height = 400, 400  # 10 čtverců po 20 pixelech v každém směru
+screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption("10x10 Grid")
+font = pygame.font.SysFont(None, 24)
 
-        neural_network = VGroup(neurons_layer1, neurons_layer2, neurons_layer3).arrange(DOWN, buff=1)
+# Barvy
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
 
-        # Adding connections
-        connections = VGroup()
-        for layer1_neuron in neurons_layer1:
-            for layer2_neuron in neurons_layer2:
-                connections.add(Line(layer1_neuron.get_center(), layer2_neuron.get_center()).set_stroke(width=0.1))
-        for layer2_neuron in neurons_layer2:
-            for layer3_neuron in neurons_layer3:
-                connections.add(Line(layer2_neuron.get_center(), layer3_neuron.get_center()).set_stroke(width=0.1))
+# Velikost jednotlivých čtverců
+cell_size = 40
 
-        # Animating the neural network
-        self.play(Create(connections), run_time=2)
-        self.play(Create(neural_network), run_time=2)
+# Hlavní smyčka
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
 
-        # Displaying text
-        description = Text("A basic representation of a Neural Network with three layers.", font_size=18)
-        self.play(Write(description))
-        self.wait(2)
-        self.play(FadeOut(description), FadeOut(neural_network), FadeOut(connections))
+    # Vymazání obrazovky
+    screen.fill(WHITE)
 
-        # Transition to the next part
-        # ...
+    # Vykreslení mřížky a textu
+    for x in range(10):
+        for y in range(10):
+            rect = pygame.Rect(x * cell_size, y * cell_size, cell_size, cell_size)
+            pygame.draw.rect(screen, BLACK, rect, 1)
+            text = font.render(f"({y},{x})", True, BLACK)
+            screen.blit(text, (x * cell_size + 5, y * cell_size + 5))
+
+    # Aktualizace obrazovky
+    pygame.display.flip()
+
+# Ukončení Pygame
+pygame.quit()
+sys.exit()
